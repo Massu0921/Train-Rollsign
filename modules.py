f77aa943dacdaa748de9615a13005ea16437b3df
Massu <imomushi.pipi@gmail.com>
2018-10-05 12:44:37 +0900

    スクロール構文追加

--- modules.py
+++ modules.py
@@ -1,3 +1,43 @@
 # -*- coding: utf-8 -*-
 from rgbmatrix import RGBMatrix, RGBMatrixOptions, graphics
 from PIL import Image, ImageDraw, ImageSequence
+
+class LED(object):
+
+    # Setup LEDs
+    def setup(self,chain=4,bright=50): # デフォルト設定（引数なしの場合）
+        # Options
+        self.options = RGBMatrixOptions()
+        self.options.rows = 32
+        self.options.chain_length = chain
+        self.options.parallel = 1
+        self.options.hardware_mapping = 'adafruit-hat-pwm'
+        self.options.brightness = bright
+        self.options.show_refresh_rate = 0
+        self.matrix = RGBMatrix(options=self.options)
+        self.canvas = self.matrix.CreateFrameCanvas()
+
+        # テキスト用フォント
+        self.gothic = graphics.Font()
+        self.gothic.LoadFont("Resources/Gothic-16.bdf")
+
+        # color
+        self.white  = graphics.Color(255, 255, 255)
+
+        # LED長さ
+        self._width  = self.canvas.width
+        self._height = self.canvas.height
+
+    def scroll_text(self):
+        x = led._width
+        while 1:
+            led.canvas.Clear()
+            len = graphics.DrawText(led.canvas, led.gothic, x, 30, led.white, text)
+            if x + len < 0:
+                break
+            x -= 1
+            time.sleep(0.01)
+            led.canvas = led.matrix.SwapOnVSync(led.canvas)
+
+        led.canvas.Clear()
+        led.canvas = led.matrix.SwapOnVSync(led.canvas)
