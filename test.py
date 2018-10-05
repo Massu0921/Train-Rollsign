005e94460cfe12ea213d5cd3cd4d6f6eb805a52b
Massu <imomushi.pipi@gmail.com>
2018-10-05 11:35:15 +0900

    スレッドを採用

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 # coding: utf-8
-import sys,os,time
-#sys.path.append(os.path.abspath(os.path.dirname(__file__) + '../'))
+import sys,os,time,threading
+sys.path.append(os.path.abspath(os.path.dirname(__file__) + '../'))
 from flask import Flask, render_template, request, redirect, url_for
-#from rgbmatrix import RGBMatrix, RGBMatrixOptions, graphics
+from rgbmatrix import RGBMatrix, RGBMatrixOptions, graphics
 from PIL import Image, ImageDraw, ImageSequence
-"""
-class Led_Setup(object):
+
+class LED(object):
 
     # Setup LEDs
-    def __init__(self,chain=4,bright=50): # デフォルト設定（引数なしの場合）
+    def setup(self,chain=4,bright=50): # デフォルト設定（引数なしの場合）
         # Options
         self.options = RGBMatrixOptions()
         self.options.rows = 32
@@ -30,44 +30,57 @@ class Led_Setup(object):
         # LED長さ
         self._width  = self.canvas.width
         self._height = self.canvas.height
-"""
-# appインスタンス生成
-app = Flask(__name__)
 
-@app.route('/')
-def index():
-    title = 'Welcome'
-    message = 'Text Message'
+class App(LED):
+    def __init__(self):
+        # appインスタンス生成
+        self.app = Flask(__name__)
 
-    return render_template('index.html',message=message,title=title)
+    @self.app.route('/')
+    def index(self):
+        title = 'Welcome'
+        message = 'Text Message'
 
-@app.route('/post',methods=['GET','POST'])
-def post():
-    title = 'Hello'
-    if request.method == 'POST':
-        name = request.form['name']
-        text = name + u'さん、ようこそ'
-        """
-        x = led._width
-        while 1:
-            led.canvas.Clear()
-            len = graphics.DrawText(led.canvas, led.gothic, x, 30, led.white, text)
-            if x + len < 0:
+        return render_template('index.html',message=message,title=title)
+
+    @self.app.route('/post',methods=['GET','POST'])
+    def post(self):
+        title = 'Hello'
+        if request.method == 'POST':
+            name = request.form['name']
+            text = name + u'さん、ようこそ'
+
+            x = led._width
+            while 1:
                 led.canvas.Clear()
+                len = graphics.DrawText(led.canvas, led.gothic, x, 30, led.white, text)
+                if x + len < 0:
+                    led.canvas.Clear()
+                    led.canvas = led.matrix.SwapOnVSync(led.canvas)
+                    break
+                x -= 1
+                time.sleep(0.01)
                 led.canvas = led.matrix.SwapOnVSync(led.canvas)
-                break
-            x -= 1
-            time.sleep(0.01)
-            led.canvas = led.matrix.SwapOnVSync(led.canvas)
-        """
 
-        # レンダリング
-        return render_template('index.html',name=name, title=title)
-    else:
-        # リダイレクト
-        return redirect(url_for('index'))
+
+            # レンダリング
+            return render_template('index.html',name=name, title=title)
+        else:
+            # リダイレクト
+            return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.debug = True
-    #led = Led_Setup()
-    app.run(host='0.0.0.0', port=8000, threaded=True)
+    led = App()
+    led.app.debug = True
+    app_th = threading.Thread(target=led.app.run,args=(host='0.0.0.0', port=8000, threaded=True,))
+    #led.app.run(host='0.0.0.0', port=8000, threaded=True)
+    app_th.setDaemon(True)
+    app_th.start()
+
+    led.setup()
+
+    try:
+        while True:
+            time.sleep(100)
+    except KeyboardInterrupt:
+        sys.exit(0)
