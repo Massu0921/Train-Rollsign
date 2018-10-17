1430c91b5b4abe1721e4afb7f85c31c3ce0b3f95
Massu <imomushi.pipi@gmail.com>
2018-10-17 22:07:49 +0900

    画像スライド仮実装

--- static/js/rollsign.js
+++ static/js/rollsign.js
@@ -1,7 +1,24 @@
 function scroll() {
     var id = $(this).attr("id")
-    alert(id)
+    //alert(id)
+    switch (id) {
+        case 'type-up':
+            $("#type").animate({ top: "-=" + (64) + "px" }, { duration: 400 }, { complete: function () { } });
+            break;
+        case 'type-down':
+            $("#type").animate({ top: "+=" + (64) + "px" }, { duration: 400 }, { complete: function () { } });
+            break;
+        case 'dest-up':
+            $("#destination").animate({ top: "-=" + (64) + "px" }, { duration: 400 }, { complete: function () { } });
+            break;
+        case 'dest-down':
+            $("#destination").animate({ top: "+=" + (64) + "px" }, { duration: 400 }, { complete: function () { } });
+            break;
+        default:
+            break;
+    }
     //$("#type").animate({ top: "-=" + (64) + "px" }, { duration: 400 }, { complete: function(){} });
 }
 
-$('.btn').on('click',scroll)
+// ボタンが押されたとき1
+$('.btn').on('click', scroll)
