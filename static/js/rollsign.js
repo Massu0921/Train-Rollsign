8f94fb6e6ae26cd41dab4ffd775f700d2bb7e2f6
Massu <imomushi.pipi@gmail.com>
2018-10-18 19:19:30 +0900

    機能追加準備

--- static/js/rollsign.js
+++ static/js/rollsign.js
@@ -1,3 +1,5 @@
+var scrolljudge = false;
+
 function scroll() {
     var id = $(this).attr("id")
     //alert(id)
@@ -20,5 +22,21 @@ function scroll() {
     //$("#type").animate({ top: "-=" + (64) + "px" }, { duration: 400 }, { complete: function(){} });
 }
 
+function longscroll() {
+    while (scrolljudge) {
+        console.log('down')
+    }
+}
+
+function btndown(){
+    scrolljudge = true;
+}
+
+function btnup(params) {
+    scrolljudge = false;
+}
+
 // ボタンが押されたとき
 $('.btn').on('click', scroll);
+$('.btn').on('mousedown', btndown);
+$('.btn').on('mouseup', x);
