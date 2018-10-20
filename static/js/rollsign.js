5bec0d28f6384c260259e0041d588b8dca05a939
Massu <imomushi.pipi@gmail.com>
2018-10-20 18:21:04 +0900

    多重リクエストを防止

--- static/js/rollsign.js
+++ static/js/rollsign.js
@@ -1,31 +1,39 @@
 // 設定
-var click_scl_px =      64;   // クリック時スクロール量
-var dblclick_scl_px =   192;  // ダブルクリック時スクロール量
-var dr =                400;  // スクロール時間(ms)
+var click_scl_px = 64;   // クリック時スクロール量
+var dblclick_scl_px = 192;  // ダブルクリック時スクロール量
+var dr = 400;  // スクロール時間(ms)
+
+// その他
+var anime_wait = true; // 多重リクエスト防止
 
 // スクロール関数(ボタンid,移動量)
 function scroll(id, scl_px) {
-    // スクロール(id振り分け)
-    switch (id) {
-        // 種別・上ボタン
-        case 'type-up':
-            $("#type").animate({ top: "-=" + (scl_px) + "px" }, { duration: dr }, { complete: function () { } });
-            break;
-        // 種別・下ボタン
-        case 'type-down':
-            $("#type").animate({ top: "+=" + (scl_px) + "px" }, { duration: dr }, { complete: function () { } });
-            break;
-        // 行先・上ボタン
-        case 'dest-up':
-            $("#destination").animate({ top: "-=" + (scl_px) + "px" }, { duration: dr }, { complete: function () { } });
-            break;
-        // 行先・下ボタン
-        case 'dest-down':
-            $("#destination").animate({ top: "+=" + (scl_px) + "px" }, { duration: dr }, { complete: function () { } });
-            break;
+    // 多重リクエスト防止
+    if (anime_wait) {
+        anime_wait = false;
+        // スクロール(id振り分け)
+        switch (id) {
+            // 種別・上ボタン
+            case 'type-up':
+                $("#type").animate({ top: "-=" + (scl_px) + "px" }, { duration: dr }, { complete: function () { } });
+                break;
+            // 種別・下ボタン
+            case 'type-down':
+                $("#type").animate({ top: "+=" + (scl_px) + "px" }, { duration: dr }, { complete: function () { } });
+                break;
+            // 行先・上ボタン
+            case 'dest-up':
+                $("#destination").animate({ top: "-=" + (scl_px) + "px" }, { duration: dr }, { complete: function () { } });
+                break;
+            // 行先・下ボタン
+            case 'dest-down':
+                $("#destination").animate({ top: "+=" + (scl_px) + "px" }, { duration: dr }, { complete: function () { } });
+                break;
 
-        default:
-            break;
+            default:
+                break;
+        }
+        anime_wait = true;
     }
     //$("#type").animate({ top: "-=" + (64) + "px" }, { duration: 400 }, { complete: function(){} });
 }
