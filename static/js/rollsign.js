0b234993670d5c00780ad48aaf3fdeb7f0aa31ad
Massu <imomushi.pipi@gmail.com>
2018-10-20 17:32:50 +0900

    ダブルクリック時の機能追加

--- static/js/rollsign.js
+++ static/js/rollsign.js
@@ -1,26 +1,59 @@
-var click_scl_px = 64;
-var dblclick_scl_px = 192;
-function scroll() {
-    var id = $(this).attr("id")
-    //alert(id)
+// 設定
+var click_scl_px =      64;   // クリック時スクロール量
+var dblclick_scl_px =   192;  // ダブルクリック時スクロール量
+var dr =                400;  // スクロール時間(ms)
+
+// スクロール関数(ボタンid,移動量)
+function scroll(id, scl_px) {
+    // スクロール(id振り分け)
     switch (id) {
+        // 種別・上ボタン
         case 'type-up':
-            $("#type").animate({ top: "-=" + (64) + "px" }, { duration: 400 }, { complete: function () { } });
+            $("#type").animate({ top: "-=" + (scl_px) + "px" }, { duration: dr }, { complete: function () { } });
             break;
+        // 種別・下ボタン
         case 'type-down':
-            $("#type").animate({ top: "+=" + (64) + "px" }, { duration: 400 }, { complete: function () { } });
+            $("#type").animate({ top: "+=" + (scl_px) + "px" }, { duration: dr }, { complete: function () { } });
             break;
+        // 行先・上ボタン
         case 'dest-up':
-            $("#destination").animate({ top: "-=" + (64) + "px" }, { duration: 400 }, { complete: function () { } });
+            $("#destination").animate({ top: "-=" + (scl_px) + "px" }, { duration: dr }, { complete: function () { } });
             break;
+        // 行先・下ボタン
         case 'dest-down':
-            $("#destination").animate({ top: "+=" + (64) + "px" }, { duration: 400 }, { complete: function () { } });
+            $("#destination").animate({ top: "+=" + (scl_px) + "px" }, { duration: dr }, { complete: function () { } });
             break;
+
         default:
             break;
     }
     //$("#type").animate({ top: "-=" + (64) + "px" }, { duration: 400 }, { complete: function(){} });
 }
 
-// ボタンが押されたとき
-$('.btn').on({ 'click': scroll }, { 'dblclick': scroll });
+// click,dblclick判定 -> スクロール実行
+var clicked = false;    // クリック状態を保持するフラグ
+
+function clickjudge() {
+    // ボタンの個別id取得
+    var id = $(this).attr("id")
+
+    if (clicked) {
+        // ダブルクリック時
+        scroll(id,dblclick_scl_px);
+        clicked = false;
+        return;
+    }
+
+    clicked = true;
+
+    setTimeout(function () {
+        if (clicked) {
+            // シングルクリック時
+            scroll(id,click_scl_px);
+        }
+        clicked = false;
+    }, 300);
+}
+
+// ボタンがクリックされたとき
+$('.btn').on('click', clickjudge);
